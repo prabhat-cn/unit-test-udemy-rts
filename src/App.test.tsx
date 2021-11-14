0d92eef0dc5ee7d687cb7c5e43532e99e8a38830
prabhat-cn <prabhat@capitalnumbers.com>
2021-11-14 14:19:27 +0530

    design & Env ready

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Unit Testing React Typescript/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from './Applications/ClickCounter/Counter';
+import Congrats from './Applications/Joto/Congrats/Congrats';
+import GuessedWords from './Applications/Joto/GuessedWords/GuessedWords';
 
-function App() {
+interface Props {}
+
+const App = (props: Props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" container App">
+      <h1>Unit Testing React Typescript</h1>
+      <br />
+      <h2>Created Applications Below</h2>
+      <Counter />
+      <br />
+      <br />
+      <h2>--Joto Guess Game--</h2>
+      <Congrats success={false} />
+      <GuessedWords
+        guessedWords={[{ guessedWord: 'train', letterMatchCount: 3 }]}
+      />
     </div>
   );
-}
+};
 
 export default App;
