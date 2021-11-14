7f0ea92415177d45d7386621a42a33e52031097a
prabhat-cn <prabhat@capitalnumbers.com>
2021-11-14 18:29:02 +0530

    input tested

--- src/Applications/Joto/InputComponent/__tests__/Input.test.tsx
+++ src/Applications/Joto/InputComponent/__tests__/Input.test.tsx
@@ -18,3 +18,20 @@ it('Input renders without error', () => {
   const inputComponent = findByTestAttr(wrapper, 'component-input');
   expect(inputComponent.length).toBe(1);
 });
+
+describe('state controlled input field', () => {
+  it('state updates with value of input box upon change', () => {
+    // if useState used
+    const mockSetCurrentGuess = jest.fn();
+    React.useState = jest.fn(() => ['', mockSetCurrentGuess]);
+
+    const wrapper = setup();
+    const inputBox = findByTestAttr(wrapper, 'input-box');
+    const mockEvent = { target: { value: 'train' } };
+    // for inputbox onChange
+    inputBox.simulate('change', mockEvent);
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+  });
+  it('', () => {});
+});
